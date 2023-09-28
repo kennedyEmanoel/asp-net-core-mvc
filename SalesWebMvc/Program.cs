f9606d5e8e6d52611acb5ab3985cdb2bc59fe62c
Kennedy Emanoel <kennedy1emanoel19@gmail.com>
2023-09-28 09:56:21 -0300

    SellersController

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,24 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
 
+// consider this section as ConfigureServices()
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddScoped<SeedingService>();
+}
 
 builder.Services.AddDbContext<SalesWebMvcContext>
     (options => options.UseMySql(
         "server=localhost;initial catalog=SALES_WEB_MVC;uid=root;pwd=1234567",
         Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql")));
 
+//builder.Services.AddTransient<SeedingService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//consider this section as Configure()
 var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseDeveloperExceptionPage();
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
